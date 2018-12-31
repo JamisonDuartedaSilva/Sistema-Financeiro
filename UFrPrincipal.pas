718b7c33725e2ea58d545f01dae9b34a0e761750
Jamison Duarte da Silva <jamisonduarte23@gmail.com>
2018-12-31 00:46:54 -0200

    Preparando e testando diretórios

--- UFrPrincipal.pas
+++ UFrPrincipal.pas
@@ -4,10 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
+  Vcl.StdCtrls;
 
 type
   TfrPrincipal = class(TForm)
+    lbCadastros: TLabel;
+    imgUsuarios: TImage;
   private
     { Private declarations }
   public
