fb42dfbca04d331094d98cdba24b073af679636b
Jamison Duarte da Silva <jamisonduarte23@gmail.com>
2018-12-31 22:24:07 -0200

    Preparando formulário principal

--- prSistemaFinancas.dpr
+++ prSistemaFinancas.dpr
@@ -2,13 +2,16 @@ program prSistemaFinancas;
 
 uses
   Vcl.Forms,
-  UFrPrincipal in 'UFrPrincipal.pas' {frPrincipal};
+  UFrPrincipal in 'UFrPrincipal.pas' {frPrincipal},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10 SlateGray');
   Application.CreateForm(TfrPrincipal, frPrincipal);
   Application.Run;
 end.
